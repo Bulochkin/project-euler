db6bb7803ccd32c5f26373e4422ce1b34e10c72e
Pavel Bulochkin <bulochkin@tut.by>
2018-06-14 16:28:35 +0300

    More friendly problem 31 solution.

--- 1-100/31-40/31.cpp
+++ 1-100/31-40/31.cpp
@@ -17,12 +17,9 @@ int main()
                         {
                             for (int p2 = 0; p2 < 201; p2 += 2)
                             {
-                                for (int p1 = 0; p1 < 201; p1 += 1)
+                                if (p200 + p100 + p50 + p20 + p10 + p5 + p2 <= 200)
                                 {
-                                    if (p200 + p100 + p50 + p20 + p10 + p5 + p2 + p1 == 200)
-                                    {
-                                        total++;
-                                    }
+                                    total++;
                                 }
                             }
                         }
@@ -32,7 +29,35 @@ int main()
         }
     }
 
-
     std::cout << total << std::endl;
     return 0;
 }
+
+/*
+
+#include <iostream>
+using namespace std;
+
+int coins[8] = { 200, 100, 50, 20, 10, 5,2,1 };
+
+int findposs(int money, int maxcoin)
+{
+    int sum = 0;
+    if (maxcoin == 7) return 1;
+    for (int i = maxcoin; i<8; i++)
+    {
+        if (money - coins[i] == 0)
+            sum += 1;
+    
+        if (money - coins[i] > 0)
+            sum += findposs(money - coins[i], i);
+    }
+    return sum;
+}
+
+int main()
+{
+    cout << findposs(200, 0) << endl;
+}
+
+*/
