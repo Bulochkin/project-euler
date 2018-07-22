21aaea81da25053cbce3e58c5c3f87cc224b18fa
bulochkin@tut.by <bulochkin@tut.by>
2018-07-23 00:11:52 +0300

    Problem 60 has been resolved.

--- 1-100/51-60/60.cpp
+++ 1-100/51-60/60.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <numeric>
 
 void sieve_eratosthene(const long long n, std::vector<long long>& primes)
 {
@@ -26,7 +27,7 @@ void sieve_eratosthene(const long long n, std::vector<long long>& primes)
     }
 }
 
-bool is_pair(const std::vector<long long> primes, long long i1, long long i2)
+bool is_pair(const std::vector<long long>& primes, long long i1, long long i2)
 {
     std::string&& s1 = std::to_string(i1);
     std::string&& s2 = std::to_string(i2);
@@ -35,63 +36,92 @@ bool is_pair(const std::vector<long long> primes, long long i1, long long i2)
             std::binary_search(primes.begin() , primes.end() , std::stoll(s2 + s1));
 }
 
-int main()
+void run_intersections3(const std::vector<long long>& primes, std::vector<std::vector<long long>>& pairs_all)
 {
-    const int border = 2000;
-    const int total = 1000000;
-
-    std::vector<long long> primes;
-    sieve_eratosthene(total, primes);
-
-    for (auto it1 = primes.begin() ; *it1 < border ; it1++)
+    std::vector<std::vector<long long>> pairs_new;
+    for (auto it1 = pairs_all.begin(); it1 != pairs_all.end(); it1++)
     {
-        std::vector<long long> pairs;
-        for (auto it2 = it1; *it2 < border ; it2++)
+        for (auto it2 = it1->begin() + 2; it2 != it1->end(); it2++)
         {
-            if (pairs.empty())
+            for (auto it3 = it2 + 1; it3 != it1->end(); it3++)
             {
-                for (auto it3 = it2; *it3 < border ; it3++)
+                for (auto it4 = it3 + 1; it4 != it1->end(); it4++)
                 {
-                    if (is_pair(primes, *it2, *it3))
+                    if (is_pair(primes, *it2, *it3) && is_pair(primes, *it3, *it4) && is_pair(primes, *it2, *it4))
                     {
-                        pairs.push_back(*it2);
-                        pairs.push_back(*it3);
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                if (std::find(pairs.begin(), pairs.end(), *it2) != pairs.end())
-                {
-                    continue;
-                }
-
-                bool is_ok = true;
-                for (auto cand : pairs)
-                {
-                    if (!is_pair(primes, cand, *it2))
-                    {
-                        is_ok = false;
-                        break;
-                    }
-                }
-
-                if (is_ok)
-                {
-                    pairs.push_back(*it2);
-                    if (pairs.size() == 4)
-                    {
-                        for (auto p : pairs)
-                        {
-                            std::cout << p << " ";
-                        }
-                        std::cout << std::endl;
-                        break;
+                        pairs_new.push_back({ it1->operator[](0), it1->operator[](1), *it2, *it3, *it4});
                     }
                 }
             }
         }
+    }
+    pairs_all = std::move(pairs_new);
+}
+
+void run_intersections(std::vector<std::vector<long long>>& pairs_all)
+{
+    std::vector<std::vector<long long>> pairs_new;
+    for (auto it1 = pairs_all.begin(); it1 != pairs_all.end(); it1++)
+    {
+        for (auto it2 = it1 + 1; it2 != pairs_all.end(); it2++)
+        {
+            std::vector<long long> pairs_one;
+
+            if (std::binary_search(it1->begin(), it1->end(), it2->operator[](0)))
+            {
+                pairs_one.push_back(it1->operator[](0));
+                pairs_one.push_back(it2->operator[](0));
+
+                std::set_intersection(it1->begin() + 1, it1->end(), it2->begin() + 1, it2->end(), std::back_inserter(pairs_one));
+
+                if (pairs_one.size() > 4)
+                {
+                    pairs_new.push_back(pairs_one);
+                }
+            }
+        }
+    }
+    pairs_all = std::move(pairs_new);
+}
+
+int main()
+{
+    const int border = 10000;
+    const int total = 100000000;
+
+    std::vector<long long> primes;
+    primes.reserve(total / 5);
+
+    sieve_eratosthene(total, primes);
+
+    std::vector<std::vector<long long>> pairs_all;
+    pairs_all.reserve(border / 5);
+
+    for (auto it1 = primes.begin(); *it1 < border; it1++)
+    {
+        std::vector<long long> pairs = { *it1 };
+        pairs.reserve(border / 100);
+
+        for (auto it2 = it1 + 1; *it2 < border; it2++)
+        {
+            if (is_pair(primes, *it1, *it2))
+            {
+                pairs.push_back(*it2);
+            }
+        }
+        pairs_all.push_back(pairs);
+    }
+
+    run_intersections(pairs_all);    
+    run_intersections3(primes, pairs_all);
+
+    for (auto& combination : pairs_all)
+    {
+        for (auto d : combination)
+        {
+            std::cout << d << "\t";
+        }
+        std::cout << "| sum = " << std::accumulate(combination.begin(), combination.end(), 0LL) << std::endl;
     }
 
     return 0;
